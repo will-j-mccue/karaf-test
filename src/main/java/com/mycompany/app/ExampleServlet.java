94d5553fce97e19bc474e20621e89d567f3cecba
Patrique Legault <patrique.legault@gmail.com>
2021-04-13 14:40:20 -0400

    Adding fix for start now

--- src/main/java/com/mycompany/app/ExampleServlet.java
+++ src/main/java/com/mycompany/app/ExampleServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @Component(
+        immediate = true,
         property = { "alias=/servlet-example", "servlet-name=Example"}
 )
 public class ExampleServlet extends HttpServlet implements Servlet {
